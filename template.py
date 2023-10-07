1d71466cb95e96a62b01f30d0144c96e64f54752
Abhikkumar619 <abisheky194@gmail.com>
2023-10-07 21:39:03 +0545

    floder structure added

---
+++ template.py
@@ -0,0 +1,48 @@
+import os 
+from pathlib import Path
+import logging
+
+
+# Logging string.
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s : %(message)s :]')
+
+project_name='cnn_classifier'
+
+list_of_file=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirement.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_file:
+    file_path=Path(filepath)
+    file_dir, file_name=os.path.split(file_path)
+
+    logging.info("splitting the directory and file.")
+    
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory :{file_dir} for file {file_name}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"creating empty file : {filepath}")
+
+    else: 
+        logging.info(f"File is already created.{filepath}")
+
+
